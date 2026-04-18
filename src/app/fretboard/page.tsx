16a8bea7b2a6ac1dae921522f1341f5f2ab1c920
Kevin Babakhani <kevinbabakhani@yahoo.com>
2026-04-17 23:54:33 -0700

    1. Major fretboard upgrades.
    2. Installed clsx and upgraded next and eslint config both to 16.2.4.

--- src/app/fretboard/page.tsx
+++ src/app/fretboard/page.tsx
@@ -1,6 +1,7 @@
 // Fretboard page for the Serenity Strings website.
 
 import React from 'react'
+import clsx from "clsx";
 
 import { Metadata } from 'next';
 
@@ -72,9 +73,10 @@ const FretboardPage = () => {
 
       {/* Main fretboard section container (the dark box) */}
       <section className = "bg-slate-800 p-6 rounded-xl border border-slate-700 mb-6">
-
+  
         {/* This wraps all 6 string rows vertically */}
-        <div className = "flex flex-col">
+        <div className = "mr-6 -mt-3">
+          <h2 className = "font-semibold mb-4 text-center text-2xl"> Fretboard </h2>
           {/* Loop 6 times to create 6 strings */}
           {
                                           // Current element value, current index 
@@ -83,30 +85,75 @@ const FretboardPage = () => {
               // Each row represents ONE string
               <div
                 key = {rowIndex} // Required by React to track elements efficiently
-                className = "grid relative" // Grid layout for frets + spacing between cells
+                className = "grid relative items-center justify-items-center" // Grid layout for frets + spacing between cells
                 
                 // create a CSS Grid container with exactly 24 columns that are all equally sized, 
                 // taking up the full width of the container.
-                style = {{ gridTemplateColumns: "repeat(24, 1fr)" }}
+                style = {{ gridTemplateColumns: "repeat(25, 1fr)" }}
               >
-              <hr className = "absolute top-4 w-full border z-10"/>
+                
+                <hr
+                className={clsx(
+                  "absolute w-full z-10 border-gray-200 ml-8",
+                  rowIndex === 0 && "border-[1px]",
+                  rowIndex === 1 && "border-[1.4px]",
+                  rowIndex === 2 && "border-[1.8px]",
+                  rowIndex === 3 && "border-[2.2px]",
+                  rowIndex === 4 && "border-[2.6px]",
+                  rowIndex === 5 && "border-[3.5px]",
+                )}
+              />
               
                 {/* Inner loop: creates 24 frets for each string */}
                 {
                                                   // Current element value, current index 
-                  Array.from({ length: 24 }).map((_, colIndex) => (
+                  Array.from({ length: 25 }).map((_, colIndex) => (
+
+                    // TODO: Add different spacing between each fret (Big -> Smaller -> Small)
+
+                    // TODO: Add fret numbers at the bottom of the fretboard (maybe only for frets 3, 5, 7, 9, 12)
 
                     // Each cell represents ONE fret position
                     <div
                       key = {colIndex} // Unique key for each cell
-                      className = "h-8 flex items-center justify-center border-l-3 border-r-3 border-gray-600"
+                      className = {clsx(
+                        "h-14 flex items-center justify-center border-r",
+                        colIndex === 0 && "border-r-8 border-black",
+                        colIndex === 1 && "border-r-2 border-gray-600",
+                        colIndex === 2 && "border-r-2 border-gray-600",
+                        colIndex === 3 && "border-r-2 border-gray-600",
+                        colIndex === 4 && "border-r-2 border-gray-600",
+
+                        colIndex === 5 && "border-r-2 border-gray-600",
+                        colIndex === 6 && "border-r-2 border-gray-600",
+                        colIndex === 7 && "border-r-2 border-gray-600",
+                        colIndex === 8 && "border-r-2 border-gray-600",
+                        colIndex === 9 && "border-r-2 border-gray-600",
+
+                        colIndex === 10 && "border-r-2 border-gray-600",
+                        colIndex === 11 && "border-r-2 border-gray-600",
+                        colIndex === 12 && "border-r-2 border-gray-600",
+                        colIndex === 13 && "border-r-2 border-gray-600",
+                        colIndex === 14 && "border-r-2 border-gray-600",
+
+                        colIndex === 15 && "border-r-2 border-gray-600",
+                        colIndex === 16 && "border-r-2 border-gray-600",
+                        colIndex === 17 && "border-r-2 border-gray-600",
+                        colIndex === 18 && "border-r-2 border-gray-600",
+                        colIndex === 19 && "border-r-2 border-gray-600",
+
+                        colIndex === 20 && "border-r-2 border-gray-600",
+                        colIndex === 21 && "border-r-2 border-gray-600",
+                        colIndex === 22 && "border-r-2 border-gray-600",
+                        colIndex === 23 && "border-r-2 border-gray-600",
+                        colIndex === 24 && "border-r-2 border-gray-600",
+                      )}
                       // bg-slate-600 → visible cell color
                       // h-6 → cell height
                       // flex + items-center + justify-center → centers text inside
                       // text-xs → small font for numbers
                     >
                     </div>
-
                   ))
                 }
 
