0bd674c65fbf560a60c23a9091d833d8af2b2d56
Kevin Babakhani <kevinbabakhani@yahoo.com>
2026-04-22 00:54:11 -0700

    Made fretboard (and the page) more responsive and clean.

--- src/app/fretboard/page.tsx
+++ src/app/fretboard/page.tsx
@@ -11,155 +11,151 @@ export const metadata: Metadata = {
 
 const FretboardPage = () => {
   return (
-    <main className = "p-8 mt-10 max-w-6xl mx-auto border border-slate-300 bg-slate-800">
+    <main className = "p-4 sm:p-6 lg:p-8 mt-10 max-w-6xl mx-auto border border-slate-300 bg-slate-800">
       <h1 className = "text-3xl font-bold mb-6"> Interactive Fretboard </h1>
 
       {/* Controls */}
-      <section className = "bg-slate-800 p-6 items-end rounded-xl border border-slate-700 mb-6">
-        <div className = "flex flex-wrap gap-4">
+      <section className = "bg-slate-800 items-end rounded-xl border border-slate-700 mb-6">
+        <div className = "flex flex-wrap gap-6 items-center justify-center">
 
-          {/* Key */}
-          <label className = "text-sm text-slate-400"> Key </label>
-          <select className = "w-42 bg-slate-700 p-2 rounded">
-            <option> C </option>
-            <option> C# </option>
-            <option> D </option>
-            <option> D# </option>
-            <option> E </option>
-            <option> F </option>
-            <option> F# </option>
-            <option> G </option>
-            <option> G# </option>
-            <option> A </option>
-            <option> A# </option>
-            <option> B </option>
-          </select>
+            {/* Key */}
+          <div className = "flex flex-col m-6">
+            <label className = "text-sm text-slate-400"> Key </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> C </option>
+              <option> C# </option>
+              <option> D </option>
+              <option> D# </option>
+              <option> E </option>
+              <option> F </option>
+              <option> F# </option>
+              <option> G </option>
+              <option> G# </option>
+              <option> A </option>
+              <option> A# </option>
+              <option> B </option>
+            </select>
+          </div>
 
-          {/* Scale */}
-          <label className = "text-sm text-slate-400"> Scale </label>
-          <select className = "w-42 bg-slate-700 p-2 rounded">
-            <option> None </option>
-            <option> Major </option>
-            <option> Minor </option>
-          </select>
+            {/* Scale */}
+          <div className = "flex flex-col m-6">
+            <label className = "text-sm text-slate-400"> Scale </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> None </option>
+              <option> Major </option>
+              <option> Minor </option>
+            </select>
+          </div>
 
-          {/* Chord */}
-          <label className = "text-sm text-slate-400"> Chord </label>
-          <select className = "w-42 bg-slate-700 p-2 rounded">
-            <option> I (Major) </option>
-            <option> ii (Minor) </option>
-            <option> iii (Minor) </option>
-            <option> IV (Major) </option>
-            <option> V (Major) </option>
-            <option> vi (Minor) </option>
-            <option> vii° (Diminished) </option>
-            <option> Dm </option>
-          </select>
+            {/* Chord */}
+          <div className = "flex flex-col m-6">
+            <label className = "text-sm text-slate-400"> Chord </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> I (Major) </option>
+              <option> ii (Minor) </option>
+              <option> iii (Minor) </option>
+              <option> IV (Major) </option>
+              <option> V (Major) </option>
+              <option> vi (Minor) </option>
+              <option> vii° (Diminished) </option>
+              <option> Dm </option>
+            </select>
+          </div>
 
-          {/* Tuning */}
-          <label className = "text-sm text-slate-400"> Tuning </label>
-          <select className = "w-42 bg-slate-700 p-2 rounded">
-            <option> Standard </option>
-            <option> Drop D </option>
-          </select>
+            {/* Tuning */}
+          <div className = "flex flex-col m-6">
+            <label className = "text-sm text-slate-400"> Tuning </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> Standard </option>
+              <option> Drop D </option>
+            </select>
+          </div>
 
-          <button className="bg-indigo-500 px-4 py-2 rounded hover:bg-indigo-400">
-            Clear
-          </button>
+          <div className = "flex flex-col m-6">
+            <button className="bg-indigo-500 px-4 mt-5 py-2 rounded hover:bg-indigo-400">
+              Clear
+            </button>
+          </div>
         </div>
       </section>
 
       {/* Fretboard Display */}
 
       {/* Main fretboard section container (the dark box) */}
-      <section className = "bg-slate-800 p-6 rounded-xl border border-slate-700 mb-6">
-  
-        {/* This wraps all 6 string rows vertically */}
-        <div className = "mr-6 -mt-3">
-          <h2 className = "font-semibold mb-4 text-center text-2xl"> Fretboard </h2>
-          {/* Loop 6 times to create 6 strings */}
-          {
-                                          // Current element value, current index 
-            Array.from({ length: 6 }).map((_, rowIndex) => (
-              
-              // Each row represents ONE string
-              <div
-                key = {rowIndex} // Required by React to track elements efficiently
-                className = "grid relative items-center justify-items-center" // Grid layout for frets + spacing between cells
-                
-                // create a CSS Grid container with exactly 24 columns that are all equally sized, 
-                // taking up the full width of the container.
-                style = {{ gridTemplateColumns: "repeat(25, 1fr)" }}
-              >
-                
-                <hr
-                className={clsx(
-                  "absolute w-full z-10 border-gray-200 ml-8",
-                  rowIndex === 0 && "border-[1px]",
-                  rowIndex === 1 && "border-[1.4px]",
-                  rowIndex === 2 && "border-[1.8px]",
-                  rowIndex === 3 && "border-[2.2px]",
-                  rowIndex === 4 && "border-[2.6px]",
-                  rowIndex === 5 && "border-[3.5px]",
-                )}
-              />
-              
-                {/* Inner loop: creates 24 frets for each string */}
-                {
-                                                  // Current element value, current index 
-                  Array.from({ length: 25 }).map((_, colIndex) => (
 
-                    // TODO: Add different spacing between each fret (Big -> Smaller -> Small)
+      {
+      // TODO: Add different spacing between each fret (Big -> Smaller -> Small)
 
-                    // TODO: Add fret numbers at the bottom of the fretboard (maybe only for frets 3, 5, 7, 9, 12)
+      // TODO: Add fret numbers at the bottom of the fretboard (maybe only for frets 3, 5, 7, 9, 12)
+      }
 
-                    // Each cell represents ONE fret position
-                    <div
-                      key = {colIndex} // Unique key for each cell
-                      className = {clsx(
-                        "h-14 flex items-center justify-center border-r",
-                        colIndex === 0 && "border-r-8 border-black",
-                        colIndex === 1 && "border-r-2 border-gray-600",
-                        colIndex === 2 && "border-r-2 border-gray-600",
-                        colIndex === 3 && "border-r-2 border-gray-600",
-                        colIndex === 4 && "border-r-2 border-gray-600",
+      {/* Fretboard Display */}
+      <section className="bg-slate-800 p-6 rounded-xl border border-slate-700 mb-6">
 
-                        colIndex === 5 && "border-r-2 border-gray-600",
-                        colIndex === 6 && "border-r-2 border-gray-600",
-                        colIndex === 7 && "border-r-2 border-gray-600",
-                        colIndex === 8 && "border-r-2 border-gray-600",
-                        colIndex === 9 && "border-r-2 border-gray-600",
+        <div className="relative flex flex-col items-center">
 
-                        colIndex === 10 && "border-r-2 border-gray-600",
-                        colIndex === 11 && "border-r-2 border-gray-600",
-                        colIndex === 12 && "border-r-2 border-gray-600",
-                        colIndex === 13 && "border-r-2 border-gray-600",
-                        colIndex === 14 && "border-r-2 border-gray-600",
+          <h2 className="font-semibold mb-4 text-center text-2xl">
+            Fretboard
+          </h2>
 
-                        colIndex === 15 && "border-r-2 border-gray-600",
-                        colIndex === 16 && "border-r-2 border-gray-600",
-                        colIndex === 17 && "border-r-2 border-gray-600",
-                        colIndex === 18 && "border-r-2 border-gray-600",
-                        colIndex === 19 && "border-r-2 border-gray-600",
+          {/* 🎯 FRETBOARD BOX */}
+          <div className="relative w-full max-w-5xl aspect-[4/1]">
 
-                        colIndex === 20 && "border-r-2 border-gray-600",
-                        colIndex === 21 && "border-r-2 border-gray-600",
-                        colIndex === 22 && "border-r-2 border-gray-600",
-                        colIndex === 23 && "border-r-2 border-gray-600",
-                        colIndex === 24 && "border-r-2 border-gray-600",
-                      )}
-                      // bg-slate-600 → visible cell color
-                      // h-6 → cell height
-                      // flex + items-center + justify-center → centers text inside
-                      // text-xs → small font for numbers
-                    >
-                    </div>
-                  ))
-                }
+            {/* 🎯 FRETS LAYER (BACKGROUND GRID) */}
 
-              </div>
-            ))
-          }
+            {/* Nut */}
+            <div className="absolute left-0 top-0 h-full bg-black z-10 w-[5px] sm:w-[7px] md:w-[9px]" />
+
+            <div
+              className="absolute inset-0 grid"
+              style={{ gridTemplateColumns: "repeat(24, 1fr)" }}
+            >
+
+              {Array.from({ length: 24 }).map((_, colIndex) => (
+                <div
+                  key={colIndex}
+                  className={clsx(
+                    "border-gray-600 border-r-2 h-full"
+                  )}
+                />
+              ))}
+            </div>
+
+            {/* 🎯 STRINGS LAYER (REAL POSITIONING) */}
+            <div className="absolute inset-0 z-0">
+
+              {Array.from({ length: 6 }).map((_, rowIndex) => {
+
+                const totalStrings = 6;
+                const spacing = 100 / totalStrings;
+
+                let topValue = spacing / 2 + rowIndex * spacing;
+
+                return (
+                  <div
+                    key={rowIndex}
+                    className={clsx(
+                      "absolute border-gray-300",
+                      rowIndex === 0 && "border",
+                      rowIndex === 1 && "border-[1.6px]",
+                      rowIndex === 2 && "border-[2px]",
+                      rowIndex === 3 && "border-[2.4px]",
+                      rowIndex === 4 && "border-[2.8px]",
+                      rowIndex === 5 && "border-[3.7px]"
+                    )}
+                    style={{
+                      top: `${topValue}%`,
+                      left: "6px",
+                      width: "calc(100% - 6px)",
+                      transform: "translateY(-4px)"
+                    }}
+                  />
+                );
+              })}
+
+            </div>
+
+          </div>
 
         </div>
       </section>
