6ce17ac03fe294b9471e72d0756775b1311f089d
Kevin Babakhani <kevinbabakhani@yahoo.com>
2026-05-23 22:04:13 -0700

    Some UI work done for Progressions page.

--- src/app/progressions/page.tsx
+++ src/app/progressions/page.tsx
@@ -76,8 +76,77 @@ const ProgressionsPage = () => {
   const getSuggestedScales = () => {};
 
   return (
-    <main className = "">
-      Progressions Page
+    <main className = "p-4 sm:p-6 lg:p-8 md:mt-5 max-w-5xl mx-auto border border-slate-300 bg-slate-800">
+      
+      {/* Controls */}
+      <section className = "bg-slate-800 p-2 rounded-xl border border-slate-700 mb-4">
+        <div className = "flex flex-wrap gap-2 md:gap-6 items-center justify-center">
+
+          {/* Key */}
+          <div className = "flex flex-col m-2 md:m-3">
+            <label className = "text-sm text-slate-400"> Key </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> C </option>
+              <option> C# </option>
+              <option> D </option>
+              <option> D# </option>
+              <option> E </option>
+              <option> F </option>
+              <option> F# </option>
+              <option> G </option>
+              <option> G# </option>
+              <option> A </option>
+              <option> A# </option>
+              <option> B </option>
+            </select>
+          </div>
+
+          {/* Mode */}
+          <div className = "flex flex-col m-2 md:m-3">
+            <label className = "text-sm text-slate-400"> Mode </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> Major </option>
+              <option> Minor </option>
+            </select>
+          </div>
+
+          {/* Mood */}
+          <div className = "flex flex-col m-2 md:m-3">
+            <label className = "text-sm text-slate-400"> Mood </label>
+            <select className = "w-full sm:w-40 bg-slate-700 p-2 rounded">
+              <option> Happy </option>
+              <option> Sad </option>
+              <option> Excited </option>
+              <option> Inspired </option>
+              <option> Silly </option>
+            </select>
+          </div>
+
+          <div className = "flex flex-col m-2 md:m-3">
+            <button className="bg-indigo-500 px-4 mt-5 py-2 rounded hover:bg-indigo-400">
+              Generate Progression
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-slate-800 p-6 rounded-xl border border-slate-700 mb-4">
+
+        <h1 className = "text-m md:text-xl font-bold mb-3"> Chords: </h1>
+        <h1 className = "text-m md:text-xl font-bold mb-3"> Roman Numerals: </h1>
+
+        <div className = "flex m-2 md:m-3">
+          <button className="bg-indigo-500 px-4 mt-5 py-2 rounded hover:bg-indigo-400">
+            Play
+          </button>
+        </div>
+
+      </section>
+
+      <section className="bg-slate-800 p-6 rounded-xl border border-slate-700 mb-4">
+        <h1 className = "text-m md:text-xl font-bold mb-3"> Suggested Scale: </h1>
+      </section>
+
     </main>
   )
 }
